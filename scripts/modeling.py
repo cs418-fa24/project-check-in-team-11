f59a9ef20b24d1fda67c28ad522a5f103d04c4d6
cd-1578 <swizz8oy@gmail.com>
2024-11-05 11:48:14 -0600

    modeling

--- scripts/modeling.py
+++ scripts/modeling.py
@@ -26,10 +26,13 @@ from sklearn.cluster import (
     DBSCAN, OPTICS, Birch, AffinityPropagation
 )
 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import json
 import pandas as pd
+import json
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
 moods = ['happy', 'sad', 'chill', 'energetic']
 dfs = []
@@ -39,7 +42,7 @@ for mood in moods:
         # f'../raw/spotify_{mood}_2.json',
         f'../raw/spotify_{mood}_3.json',
         f'../raw/spotify_{mood}_4.json',
-        # f'../raw/spotify_{mood}_5.json',
+        f'../raw/spotify_{mood}_5.json',
         # f'../raw/spotify_{mood}_6.json',
     ]
 
@@ -49,25 +52,31 @@ for mood in moods:
             df['mood'] = mood
             dfs.append(df)
 
-
 final_df = pd.concat(dfs, ignore_index=True)
 
 #processing
 drop = ['name', 'id']
 final_df = final_df.drop(columns=drop)
 
+final_df.to_csv('training.csv')
+
 #splitting
 X = final_df.iloc[:, 0:13]
 Y = final_df.iloc[:, 13]
-
+# encoder = LabelEncoder()
+# y = encoder.fit_transform(Y)
 xtrain, xtest, ytrain, ytest = train_test_split(X, Y, test_size=.2, random_state=1)
 
-# train
-forest = RandomForestClassifier()
-forest.fit(xtrain, ytrain)
+# k-fold
+k = 10
+forest = RandomForestClassifier(random_state=2)
+scores = cross_val_score(forest, xtrain, ytrain, cv=k)
+print('CV scores', scores)
+print('Mean CV scores', np.mean(scores))
 
-# score
-# print(forest.score(xtest, ytest))
+# single
+forest.fit(xtrain, ytrain)
+print('Fit Score', forest.score(xtest, ytest))
 
 ################################################################################################
 #analysis
@@ -91,28 +100,22 @@ for file in files:
         df = pd.DataFrame(json.load(fileio))
         dfs.append(df)
 
-dfs[0].to_csv('testing.csv')
-
 #processing
 drop = ['name', 'id']
-for df in dfs:
-    df = df.drop(columns=drop)
+for i in range(0, 3):
+    dfs[i] = dfs[i].drop(columns=drop)
 
 predictions = []
 
 for df in dfs:
     predictions.append(forest.predict(df.iloc[:, :]))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+person = 1
+for prediction in predictions:
+    print(f'Person {person}')
+    print('Happy', (prediction.tolist().count('happy')/len(prediction.tolist()))*100)
+    print('Sad', (prediction.tolist().count('sad')/len(prediction.tolist()))*100)
+    print('Chill', (prediction.tolist().count('chill')/len(prediction.tolist()))*100)
+    print('Energetic', (prediction.tolist().count('energetic')/len(prediction.tolist()))*100)
+    print()
+    person += 1
